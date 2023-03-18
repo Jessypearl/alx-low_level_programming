6250f5c04598b0a6803eb2afc058ac50f7932ca9
Jessypearl <Pinaoweijessica@yahoo.com>
2023-03-17 20:19:07 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 
 /**
  * main -prints exactly and that piece of art is useful
- *Return: Always 0 (Success)
+ *Return: Always 1 (Success)
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+fput("and that piece of art is useful\" - Dora korpar, 2015-10-19\n", stdout);
 return (1);
 }
