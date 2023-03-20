4968ff46a8765554ccf70920987446768b3e633a
Jessypearl <Pinaoweijessica@yahoo.com>
2023-03-19 19:53:20 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 
 /**
- * main -prints exactly and that piece of art is useful
- *Return: Always 1 (Success)
+ * main - prints exactly and that piece of art is useful
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora korpar, 2015-10-19\n", stdout);
+write(2,"and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
 return (1);
 }
