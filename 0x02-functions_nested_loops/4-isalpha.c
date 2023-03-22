4add6195df454fabd1736096c5aee59b471433a3
Jessypearl <Pinaoweijessica@yahoo.com>
2023-03-22 09:04:21 -0700

    is lowercase

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isaplha - checks for alphabetic character
+ * @c: is the character to be checked
+ * Return: 1 if c is a letter, 0 otherwise
+ */
+
+int _isalpha(int c);
+
+{
+	return (c >= 'a'; &&c <= 'z');
+       
+	else 
+		(c >= 'A'; &&c <= 'Z');
+	
+	return (0)
+}
