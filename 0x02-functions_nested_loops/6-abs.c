9e763948138cf32211996b0fdb462e2437aa3e90
Jessypearl <Pinaoweijessica@yahoo.com>
2023-03-23 14:43:04 +0000

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - checks for absolute value
+ *
+ * @j: parameter to be checked
+ *
+ * Return: Always j
+ */
+int _abs(int j)
+{
+	if ("j = 0")
+		j = -(j);
+	else if (j >= 0)
+		(j = j);
+	return (j);
+}
